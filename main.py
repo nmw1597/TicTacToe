82c29dec185d8a4fd16a482e65ba93a372902a27
nmw1597 <normanwong85@gmail.com>
2022-01-20 12:20:39 -0800

    updated game_board() and added choose_xo()

--- main.py
+++ main.py
@@ -12,39 +12,44 @@
 #Create Game board
 def game_board():
 
-    # 1'         |         |         '
-    # 2'   {7}   |   {8}   |   {9}   '
-    # 3'_________|_________|_________'
-    # 4'         |         |         '
-    # 5'   {4}   |   {5}   |   {6}   '
-    # 6'_________|_________|_________'
-    # 7'         |         |         '
-    # 8'   {1}   |   {2}   |   {3}   '
-    # 9'         |         |         '
+    print("Each number represents the square\n     |     |\n  7  |  8  |  9\n_____|_____|_____\n     |     |\n  4  |  5  |  6\n_____|_____|_____\n     |     |\n  1  |  2  |  3\n     |     |     \n\n")
 
-    # row1_4_7 = ['         |         |         ']
-    # row3_6 = ['_________|_________|_________']
-    # row9 = ['         |         |         ']
-    # row2 = ['   {7}   |   {8}   |   {9}   ']
-    # row5 = ['   {4}   |   {5}   |   {6}   ']
-    # row8 = ['   {1}   |   {2}   |   {3}   ']
-    row1_4_7 = ['     |     |     ']
+    square = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+
+    row1_4_7_9 = ['     |     |     ']
     row3_6 = ['_____|_____|_____']
-    row9 = ['     |     |     ']
-    row2 = [' ',7,' | ',8,' | ',9,' ']
-    row5 = [' {4} | {5} | {6} ']
-    row8 = [' {1} | {2} | {3} ']
+    row2 = ['  ' + square[6] + '  |  ' + square[7] + '  |  ' + square[8]]
+    row5 = ['  ' + square[5] + '  |  ' + square[4] + '  |  ' + square[3]]
+    row8 = ['  ' + square[2] + '  |  ' + square[1] + '  |  ' + square[0]]
 
-    print(row1_4_7)
-    print(row2)
-    print(row3_6)
-    print(row1_4_7)
-    print(row5)
-    print(row3_6)
-    print(row1_4_7)
-    print(row8)
-    print(row9)
-
-    print(type(row2))
+    print("Current Game Board")
+    print(''.join(row1_4_7_9))
+    print(''.join(row2))
+    print(''.join(row3_6))
+    print(''.join(row1_4_7_9))
+    print(''.join(row5))
+    print(''.join(row3_6))
+    print(''.join(row1_4_7_9))
+    print(''.join(row8))
+    print(''.join(row1_4_7_9))
 
 game_board()
+
+
+def choose_xo():
+    p1 = ''
+    p1_choice = False
+    acceptable_p1_choice = ['x', 'o', 'X', 'O']
+
+    while p1_choice == False:
+
+        p1 = input("Player 1, would you like to be X or O?")
+
+        if p1 not in acceptable_p1_choice:
+            print("Please select X or O")
+        else:
+            p1 = p1.capitalize()
+            print(f'"You have chosen {p1}')
+            p1_choice = True
+
+    return p1.capitalize()
